8dd35a31f7e04d93e04c4320ffe16ff99a366308
Felipe Ribeiro <piporibeiro@gmail.com>
2021-03-16 21:38:44 -0300

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import {Header} from "./components/Header";
 import { GlobalStyles } from './styles/global'
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-        <GlobalStyles/>
-    </div>
+    <>
+      <Header/>
+      <GlobalStyles/>
+    </>
   );
 }
 
