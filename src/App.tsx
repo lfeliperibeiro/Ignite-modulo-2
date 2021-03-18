9823f5b3bded9ec7a145b558b092a9cde3e574f2
Felipe Ribeiro <piporibeiro@gmail.com>
2021-03-17 23:38:01 -0300

    feat: add tables and button function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import {Header} from "./components/Header";
 import { GlobalStyles } from './styles/global'
 import {Dashboard} from "./components/Dashboard";
+import Modal from "react-modal";
+import {useState} from "react";
+import {NewTransactionModal} from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
+      <NewTransactionModal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyles/>
     </>
   );
